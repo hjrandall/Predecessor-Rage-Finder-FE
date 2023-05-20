ebd79495a5471b819108fbcd187b91a7b6abc489
Hayden Randall <haydenjrandall@gmail.com>
2023-05-20 14:23:31 -0600

    made changes to the submit rager page

--- GamerRageCheck/src/views/SubmitView.vue
+++ GamerRageCheck/src/views/SubmitView.vue
@@ -1,29 +1,32 @@
 <template>
     <div class="submit">
-      <v-card
-    width="600"
-    title="Submit a Rager"
-  >
+      <v-card width="600" style="box-shadow: 0 0 1000px rgb(0, 189, 126)">
+      <v-card-title style="color: rgb(0, 189, 126) ;">Submit a Rager</v-card-title>
     <v-container>
       <v-text-field
+        v-model="this.playerName"
         color="primary"
         label="Player name"
         variant="underlined"
       ></v-text-field>
 
       <v-text-field
+        v-model="this.recordingID"
         color="primary"
         label="Recording ID"
         variant="underlined"
       ></v-text-field>
 
-      <v-text-field
+      <v-select
+        v-model="this.game"
         color="primary"
-        label="Game Name"
+        label="Game"
+        :items="gameList"
         variant="underlined"
-      ></v-text-field>
-
+      ></v-select>
+        
       <v-textarea
+        v-model="this.reasonsForReport"
         color="primary"
         label="Reason for report"
         variant="underlined"
@@ -35,9 +38,8 @@
     <v-card-actions>
       <v-spacer></v-spacer>
 
-      <v-btn color="success">
+      <v-btn :disabled="isDisabled" @click="onsubmit" id="submitButton">
         Submit
-        <v-icon icon="mdi-chevron-right" end></v-icon>
       </v-btn>
     </v-card-actions>
   </v-card>
@@ -45,6 +47,12 @@
   </template>
   
   <style>
+  #submitButton {
+      width: 100%;
+      border: 1px solid rgb(0, 189, 126);
+      text-align: center;
+      color: rgb(0, 189, 126);
+    }
   @media (min-width: 1024px) {
     .submit {
       min-height: 100vh;
@@ -53,3 +61,31 @@
     }
   }
   </style>
+
+<script>
+export default {
+  data: () => ({
+    gameList: ['','Predecessor'],
+    game: "",
+    playerName: "",
+    recordingID: "",
+    reasonsForReport: "",
+  }),
+  computed: {
+    isDisabled() {
+      if (this.game === "" || this.game == null || this.playerName === "" || this.recordingID === "" || this.reasonsForReport == "") {
+          return true;
+        }
+        return false;
+        }
+},
+  methods:{
+    onsubmit(){
+      console.log(this.game)
+      console.log(this.playerName)
+      console.log(this.recordingID)
+      console.log(this.reasonsForReport)
+    }
+    },
+}
+</script>
